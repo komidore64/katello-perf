9102b8df3bcd404f36b6c67cec0e855a2561652d
Adam Price <komidore64@gmail.com>
2016-01-21 16:14:35 -0500

    uniform bash variables

--- create-katello-2.4-os1.sh
+++ create-katello-2.4-os1.sh
@@ -18,7 +18,7 @@
 set -x
 set -e
 
-readonly SERVER_NAME="$OS_USERNAME-katello2.4-perf"
+readonly SERVER_NAME="${OS_USERNAME}-katello2.4-perf"
 
 # source them-there OS env variables
 if ! env | grep ^OS &>/dev/null; then
@@ -40,41 +40,43 @@ if [ -z ${PORTAL_PASS+x} ]; then
 	read -sr PORTAL_PASS
 fi
 
-openstack server create $SERVER_NAME \
+openstack server create ${SERVER_NAME} \
 	--flavor c3.mxlarge \
 	--image _OS1_rhel-guest-image-7.2-20151102.0.x86_64.qcow2 \
-	--key-name $OPENSTACK_KEY_NAME \
+	--key-name ${OPENSTACK_KEY_NAME} \
 	--security-group default \
 	--wait
 
-readonly SERVER_ADDRESS="$( openstack server show $SERVER_NAME --format shell --column addresses \
+readonly SERVER_ADDRESS="$( openstack server show ${SERVER_NAME} --format shell --column addresses \
 	| sed 's/.*"\(.*\)".*/\1/' \
 	| cut -d= -f2 \
 	| cut -d' ' -f 2 \
 )"
 
+[ -z "${SERVER_ADDRESS}" ] && exit 1 # bail if we don't get the IP address
+
 SSH_UP=1
-while [ $SSH_UP -gt 0 ]; do
-	if nmap -p22 $SERVER_ADDRESS -oG - | grep -q 22/open; then
+while [ ${SSH_UP} -gt 0 ]; do
+	if nmap -p22 ${SERVER_ADDRESS} -oG - | grep -q 22/open; then
 		SSH_UP=0
 	fi
 done
 
-ssh-keyscan -H $SERVER_ADDRESS >> ~/.ssh/known_hosts
+ssh-keyscan -H ${SERVER_ADDRESS} >> ~/.ssh/known_hosts
 
-ssh -t cloud-user@$SERVER_ADDRESS sudo subscription-manager register \
-	--username $PORTAL_USERNAME \
-	--password $PORTAL_PASS \
+ssh -t cloud-user@${SERVER_ADDRESS} sudo subscription-manager register \
+	--username ${PORTAL_USERNAME} \
+	--password ${PORTAL_PASS} \
 	--auto-attach
 
-ssh -t cloud-user@$SERVER_ADDRESS sudo sed -i \'s/enabled = 1/enabled = 0/\' /etc/yum.repos.d/redhat.repo
+ssh -t cloud-user@${SERVER_ADDRESS} sudo sed -i \'s/enabled = 1/enabled = 0/\' /etc/yum.repos.d/redhat.repo
 
-ssh -t cloud-user@$SERVER_ADDRESS sudo yum-config-manager \
+ssh -t cloud-user@${SERVER_ADDRESS} sudo yum-config-manager \
 	--enable rhel-7-server-rpms \
 	--save
 
 # install katello 2.4 yall
-ssh -tt cloud-user@$SERVER_ADDRESS <<-END_OF_SHELL
+ssh -tt cloud-user@${SERVER_ADDRESS} <<-END_OF_SHELL
 	sudo su -l <<-END_OF_ROOT
 		cd # go home, lassie
 		yum install -y git ruby
@@ -83,4 +85,5 @@ ssh -tt cloud-user@$SERVER_ADDRESS <<-END_OF_SHELL
 		./setup.rb --version 2.4
 		exit
 	END_OF_ROOT
+	exit
 END_OF_SHELL
