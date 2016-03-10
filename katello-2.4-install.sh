9093149461e40f6bef0290bb77a57191d96cc7c7
Adam Price <komidore64@gmail.com>
2016-03-10 15:04:13 -0500

    enable httpd scoreboard on katello 2.4 install

--- katello-2.4-install.sh
+++ katello-2.4-install.sh
@@ -8,7 +8,13 @@ yum install -y ruby git
 git clone https://github.com/Katello/katello-deploy.git
 pushd katello-deploy
 git checkout KATELLO-2.4
-./setup.rb --version 2.4
+./setup.rb --version 2.4 --skip-installer
+
+# hax
+sed -i 's/include ::apache::mod::passenger/&\n  include ::apache::mod::status/' \
+	/usr/share/katello-installer/modules/foreman/manifests/config/passenger.pp
+
+./setub.rb --version 2.4
 popd
 
 ruby -rpsych -e "p Psych.load_file('/etc/katello-installer/answers.katello-installer.yaml')['foreman']['admin_password']" \
