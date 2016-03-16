7c87dd149837355f3b83d9208b7cfc0a901cea9c
Adam Price <komidore64@gmail.com>
2016-03-16 14:27:28 -0400

    adding reformat to katello-2.4-install.sh for beaker machines

--- katello-2.4-install.sh
+++ katello-2.4-install.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
 # vim: noet
 
+# ####################
+# CAUTION - This has only been tested on Beaker machines running Centos7. Run
+# at your own risk.
+# ####################
+
 set -x
 
-cd
+cd # go home
+
+# repartition
+tar cvzf $HOME/home.tgz /home
+umount /home
+lvremove --force /dev/mapper/*home
+sed -i '/home/d' /etc/fstab
+lvresize --force --extents +100%FREE /dev/mapper/*root
+resize2fs --force /dev/mapper/*root
+tar xvzf $HOME/home.tgz --directory /
+
+# install katello 2.4
 yum install -y ruby git
 git clone https://github.com/Katello/katello-deploy.git
 pushd katello-deploy
@@ -17,6 +33,7 @@ sed -i 's/include ::apache::mod::passenger/&\n  include ::apache::mod::status/' 
 ./setup.rb --version 2.4
 popd
 
+# get katello admin password
 ruby -rpsych -e "p Psych.load_file('/etc/katello-installer/answers.katello-installer.yaml')['foreman']['admin_password']" \
-  | tr -d '"' \
-  | tee $HOME/admin_password
+	| tr -d '"' \
+	| tee $HOME/admin_password
